a64d21a25f1c7bbe749c6b3d043f1d4d7ad1e039
Dev Ferreira <devferreirag@192.168.0.32>
2025-03-01 19:18:27 -0800

    Implementa funcionalidades de história (undo/redo) e tema claro/escuro

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,43 @@
 module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    '@typescript-eslint',
+    'react-refresh'
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/no-explicit-any': 'error',
+    'react/react-in-jsx-scope': 'off'
+  },
   overrides: [
     {
       files: ['*.cjs'],
@@ -11,4 +50,4 @@ module.exports = {
       }
     }
   ]
-}; 
+};
